4bdd3a55ee565b7b2f05673fb5799468e8332c9d
John Wilkins <80089266+wilkinsjohnstanley@users.noreply.github.com>
2023-09-28 16:14:37 -0500

    Create switch.cs

---
+++ switch.cs
@@ -0,0 +1,48 @@
+switch(expression) 
+{
+  case x:
+    // code block
+    break;
+  case y:
+    // code block
+    break;
+  default:
+    // code block
+    break;
+}
+/*
+This is how it works:
+
+    The switch expression is evaluated once
+    The value of the expression is compared with the values of each case
+    If there is a match, the associated block of code is executed
+    The break and default keywords will be described later in this chapter
+
+The example below uses the weekday number to calculate the weekday name:
+*/
+int day = 4;
+switch (day) 
+{
+  case 1:
+    Console.WriteLine("Monday");
+    break;
+  case 2:
+    Console.WriteLine("Tuesday");
+    break;
+  case 3:
+    Console.WriteLine("Wednesday");
+    break;
+  case 4:
+    Console.WriteLine("Thursday");
+    break;
+  case 5:
+    Console.WriteLine("Friday");
+    break;
+  case 6:
+    Console.WriteLine("Saturday");
+    break;
+  case 7:
+    Console.WriteLine("Sunday");
+    break;
+}
+// Outputs "Thursday" (day 4)
