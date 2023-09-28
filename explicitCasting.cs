78d66114e407e28dcb5d518b0d01bd321bff2702
John Wilkins <80089266+wilkinsjohnstanley@users.noreply.github.com>
2023-09-28 16:12:38 -0500

    Create explicitCasting.cs

---
+++ explicitCasting.cs
@@ -0,0 +1,5 @@
+double myDouble = 9.78;
+int myInt = (int) myDouble;    // Manual casting: double to int
+
+Console.WriteLine(myDouble);   // Outputs 9.78
+Console.WriteLine(myInt);      // Outputs 9
