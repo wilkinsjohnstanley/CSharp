ce967337f63aa7587ce4621867738ccb56f7af1b
John Wilkins <80089266+wilkinsjohnstanley@users.noreply.github.com>
2023-09-29 22:03:47 -0500

    Create arrayAccess.cs

---
+++ arrayAccess.cs
@@ -0,0 +1,12 @@
+/*
+Access the Elements of an Array
+
+You access an array element by referring to the index number.
+
+This statement accesses the value of the first element in cars:
+Example
+Get your own C# Server
+*/
+string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+Console.WriteLine(cars[0]);
+// Outputs Volvo
