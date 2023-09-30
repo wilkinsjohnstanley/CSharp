3cfe500446189503b3d4bd799fe31cf618353660
John Wilkins <80089266+wilkinsjohnstanley@users.noreply.github.com>
2023-09-29 22:03:06 -0500

    Create arrays.cs

---
+++ arrays.cs
@@ -0,0 +1,8 @@
+//To declare an array, define the variable type with square brackets:
+string[] cars;
+//To insert values to it, we can use an array literal - place the values in a comma-separated list, inside curly braces:
+
+string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+
+
