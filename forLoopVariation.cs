433833bfb1e604f87a1f348b4accd892120c354e
John Wilkins <80089266+wilkinsjohnstanley@users.noreply.github.com>
2023-10-03 12:28:54 -0500

    Create forLoopVariation.cs

---
+++ forLoopVariation.cs
@@ -0,0 +1,24 @@
+/*
+Although the three sections of the for loop 
+are most commonly used for initializing a variable, 
+testing it, and incrementing it, 
+you can also perform other tasks:
+
+    You can initialize more than one variable 
+    by placing commas between separate initializations 
+    in the first section of the for statement, 
+    as in the following:
+*/
+for(g = 0; h = 1; g < 6; ++g)
+
+//You can declare a new variable within the for statement
+for(int k = 0; k <5; ++k)
+
+//You can perform more than onoe test in the middle section 
+  for(g=0;g<3 && h >1; ++g)
+
+//You can decrement
+    for(g=5; g>=1; --g)
+// you can increment twice
+for(g=0;g<5; ++g; ++h)
+  
