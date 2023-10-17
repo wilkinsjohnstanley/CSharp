3ed8b58c53090b3b2fd08607683b15ee4fff1d50
John Wilkins <80089266+wilkinsjohnstanley@users.noreply.github.com>
2023-10-17 17:54:07 -0500

    Update CaseProblem5_1.cs

--- CaseProblem5_1.cs
+++ CaseProblem5_1.cs
@@ -21,18 +21,18 @@ class GreenvilleRevenue
       Write("Enter number of contestants last year >> ");
       entryString = ReadLine();
       numLastYear = Convert.ToInt32(entryString);
-      if (numLastYear >= 0 && numLastYear <= 30{
+      if (numLastYear >= 0 && numLastYear <= 30){
          Write("Enter a valid number between 0 and 30");
-         Write("Enter number of contestants last year >> ");
+         Write("Enter number of contestants last year ");
       entryString = ReadLine();
       numLastYear = Convert.ToInt32(entryString);
       } else 
-      Write("Enter number of contestants this year >> ");
+      Write("Enter number of contestants this year ");
       entryString = ReadLine();
       numThisYear = Convert.ToInt32(entryString);
-      if (numThisYear >= 0 && numThisYear <= 30{
+      if (numThisYear >= 0 && numThisYear <= 30){
          Write("Enter a valid number between 0 and 30");
-         Write("Enter number of contestants last year >> ");
+         Write("Enter number of contestants last year ");
       entryString = ReadLine();
       numLastYear = Convert.ToInt32(entryString);
       }
