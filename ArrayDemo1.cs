b83e955d008c36b89fa26eb15c5002345b714c10
John Wilkins <80089266+wilkinsjohnstanley@users.noreply.github.com>
2023-10-29 15:48:41 -0500

    Add files via upload

---
+++ ArrayDemo1.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+class ArrayDemo1
+
+{
+	static void Main()
+	{
+		double[] payRate = { 13.00, 17.35, 19.12, 22.45 };
+		for(int x = 0; x < payRate.Length;  x++)
+		{
+			WriteLine("Pay rate {0} is {1}",
+				x, payRate[x].ToString("C"));
+		}
+	}
+}
---
+++ ArrayDemo2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using static System.Console;
+
+class ArrayDemo2
+
+{
+    static void Main()
+    {
+        int[] scores = new int[8];
+        int x;
+        string inputString;
+
+        int count;
+        const int DASHES = 50;
+
+        //Add a loop that prompts the user, accepts a test score, converts the score to an integer, and stores it as the appropriate element of the scores array.
+        for (x = 0; x < scores.Length; ++x)
+        {
+            Write("Enter your score on test {0} ", x + 1);
+            inputString = ReadLine();
+            scores[x] = Convert.ToInt32(inputString);
+        }
+        WriteLine();
+        for (count = 0; count < DASHES; count++)
+            Write("-");
+        WriteLine();
+
+        WriteLine("Scores in original order:");
+        for(x=0; x < scores.Length;  x++)
+        {
+            Write("{0, 6}", scores[x]);
+            WriteLine();
+        }
+
+        for (count = 0; count < DASHES; count++)
+            Write("-");
+        WriteLine();
+        Array.Sort(scores);
+        WriteLine("Scores in sorted order:");
+        for (x = 0; x < scores.Length; ++x)
+            Write("{0,6}", scores[x]);
+        WriteLine();
+
+        for (count = 0; count < DASHES; count++)
+            Write("-");
+        WriteLine();
+        Array.Reverse(scores);
+        WriteLine("Scores in reverse order:");
+        for (x = 0; x < scores.Length; ++x)
+            Write("{0,6}", scores[x]);
+        WriteLine();
+    }
+}
